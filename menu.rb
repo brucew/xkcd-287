e350fb86e957786d69858e9abacfbffcee69db7f
Bruce White <bruce@homecca.com>
2013-03-30 00:09:35 -0700

    WIP

--- menu.rb
+++ menu.rb
@@ -2,22 +2,23 @@ require_relative 'helpers'
 class Menu
   attr_accessor :target_price
   attr_accessor :items
-  attr_reader   :combinations
+  attr_reader :combinations
+
   def initialize(csv_file = nil)
     @target_price = 0
-    @items        = []
+    @items = []
     self.populate_data!(csv_file) if csv_file
   end
 
   def populate_data!(csv_file)
     self.items = []
-    File.open(csv_file).each_with_index do |line, index|      
+    File.open(csv_file).each_with_index do |line, index|
       if index == 0
         self.target_price = parse_price(line)
       else
-        item    = line.split(',')
+        item = line.split(',')
         item[1] = parse_price(item[1])
-        item    = {name: item[0], price: item[1]}
+        item = {name: item[0], price: item[1]}
         self.items << item
       end
     end
@@ -25,40 +26,62 @@ class Menu
     return self
   end
 
-  def get_combos
-    @combinations = []
-    #First round of guessing: Make combinations
-    #BETER IDEA: make each possible combo an array of 6 duplicate menu items so that more combinations are possible.
-    #More resource intensive but more accurate
-    (1...(@items.count)).each do |count|
-        @items.combination(count).to_a.each do |combo_nominee|
-          @combinations << combo_nominee if combo_nominee.map{|x| x[:price]}.inject(:+) == @target_price
-        end
-    end
-
-    #second round of guessing: add multiples together until they == @target price
-    (0...(@items.count)).each do |item_index|
-      if (@target_price % @items[item_index][:price]) == 0
-        quatity = 0
-        until ( @items[item_index][:price] * quatity ) == @target_price
-          quatity = quatity + 1
-        end
-        @combinations << [@items[item_index]]*quatity
+  def find_combinations_to_price(items, target_price)
+    combinations = []
+    items.each do |item|
+      case
+        when item[:price] < target_price
+          combinations += find_combinations_to_price(items, target_price - item[:price]).collect {|combo| increment_item_count(item[:name], combo) }
+        when item[:price] == target_price
+          combinations << {item[:name] => 1}
       end
     end
+    return combinations
+  end
+
+  def increment_item_count(item_name, combination)
+    combination[item_name] = combination[item_name].nil? ? 1 : combination[item_name] + 1
+    return combination
+  end
+
+  def get_combos
+    @combinations = find_combinations_to_price(@items, @target_price)
+
+    #@combinations = []
+
+    ##First round of guessing: Make combinations
+    ##BETER IDEA: make each possible combo an array of 6 duplicate menu items so that more combinations are possible.
+    ##More resource intensive but more accurate
+    #(1...(@items.count)).each do |count|
+    #    @items.combination(count).to_a.each do |combo_nominee|
+    #      @combinations << combo_nominee if combo_nominee.map{|x| x[:price]}.inject(:+) == @target_price
+    #    end
+    #end
+    #
+    ##second round of guessing: add multiples together until they == @target price
+    #(0...(@items.count)).each do |item_index|
+    #  if (@target_price % @items[item_index][:price]) == 0
+    #    quatity = 0
+    #    until ( @items[item_index][:price] * quatity ) == @target_price
+    #      quatity = quatity + 1
+    #    end
+    #    @combinations << [@items[item_index]]*quatity
+    #  end
+    #end
 
   end
 
   def show_results
+    binding.pry
     results = ""
     if (@combinations != [])
-    results << "Greetings. Here are your #{@combinations.count} winning menu combinations\n"
+      results << "Greetings. Here are your #{@combinations.count} winning menu combinations\n"
       @combinations.each do |x|
-        x.each{|y|results << y[:name] + "\n"}
+        x.each { |y| results << y[:name] + "\n" }
         results << "==========\n"
       end
     else
-    results << "Looks like we can't make anything happen with the current menu / budget. Sorry.\n"
+      results << "Looks like we can't make anything happen with the current menu / budget. Sorry.\n"
     end
     results
   end
